daa9ba0ce3f12226516143200a830e9aac78aeff
dntjr4772 <dntjr4772@naver.com>
2022-09-15 10:06:19 +0900

    add @ConfigurationPropertiesScan

--- study/src/main/java/com/example/study/config/PropertiesConfiguration.java
+++ study/src/main/java/com/example/study/config/PropertiesConfiguration.java
@@ -4,6 +4,8 @@ import com.example.study.domain.ConstructorProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+
+
 @Configuration
 @EnableConfigurationProperties(value = {ConstructorProperties.class})
 public class PropertiesConfiguration {
--- study/src/test/java/com/example/study/service/SpELServiceTest.java
+++ study/src/test/java/com/example/study/service/SpELServiceTest.java
@@ -13,18 +13,13 @@ class SpELServiceTest {
     @Value("#{1 eq 1}")
     private boolean spelBoolean;
 
-    @Value("#{1 eq 1}")
-    private String spelString;
-
     @Value("#{externalService.apiName}")
     private String spelName;
 
     @DisplayName("@Value 를 이용한 properties bind")
     @Test
     void valueBindTest() {
-        System.out.println("spelName = " + spelName);
         assertThat(spelBoolean).isTrue();
-        assertThat(spelString).isEqualTo("true");
         assertThat(spelName).isEqualTo("lgcns");
     }
 }
